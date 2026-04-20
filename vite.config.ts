dab41287175ba3ad1809243ef3f45034b23ac23e
Faris Zaidan Nafis <farisznafis14@gmail.com>
2026-04-20 11:02:29 -0500

    fix: update base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
+  plugins: [react()];
   base: "/happy-birthday/";
   const env = loadEnv(mode, ".", "");
   return {
